9cd49c6c3aceeb15dd7386df8c4221414c3c8f80
Philip Paeps <philip@trouble.is>
2021-10-26 12:47:23 +0800

    Import rpki.net datetime handling
    Add the rpki.sundial implementation from the rpki.net Python 2 with
    minor modifications for Python 3.  Teach rpki.POW to use this class
    for all its datetime handling needs.  Very lightly tested.

--- rpki/POW/__init__.py
+++ rpki/POW/__init__.py
@@ -1,2 +1,8 @@
 from ._POW import *
 from ._POW import __doc__
+
+# Set callback to let POW construct rpki.sundial.datetime objects.
+
+from rpki.sundial import datetime as sundial_datetime
+customDatetime(sundial_datetime)
+del sundial_datetime
